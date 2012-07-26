a38170553577e2ba317c25d350ab915a2b081416
Ben Griffiths <mrbengriffiths@googlemail.com>
2012-07-26 22:40:18 +0100

    Removed compojure testing code

--- project.clj
+++ project.clj
@@ -1,10 +1,5 @@
-(defproject multipart "0.0.1-SNAPSHOT"
-  :description "Testing out multipart data in compojure"
+(defproject com.floatbackwards/multipart "0.0.1"
+  :description "Ring middleware for parsing multipart/mixed requests"
+  :url "https://github.com/whostolebenfrog/compojure-multipart"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.0"]
-                 [enlive "1.0.1"]
-                 [org.apache.commons/commons-email "1.2"]]
-  :plugins [[lein-ring "0.7.1"]
-             [jonase/kibit "0.0.4"]
-             [lein-swank "1.4.4"]]
-  :ring {:handler multipart.core/app})
+                 [org.apache.commons/commons-email "1.2"]])
