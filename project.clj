daac2d2349a9ac27976a6078aae9da55578de582
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-01-02 15:35:14 +0000

    lein-release plugin: preparing 0.0.6 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.floatbackwards/multipart "0.0.5"
+(defproject com.floatbackwards/multipart "0.0.6"
   :description "Ring middleware for parsing multipart/mixed requests"
   :url "https://github.com/whostolebenfrog/compojure-multipart"
   :dependencies [[org.clojure/clojure "1.4.0"]
@@ -6,5 +6,12 @@
                  [org.apache.commons/commons-email "1.2"]
                  [commons-fileupload/commons-fileupload "1.2.2"]]
   :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+
+  :plugins [[lein-release "1.0.0"]]
+
+  :lein-release {:release-tasks [:clean :uberjar :pom]
+                 :clojars-url "clojars@clojars.org:"}
+
+  :uberjar-name "multipart.jar"
 
   :aot [ring.TooMuchContent])
