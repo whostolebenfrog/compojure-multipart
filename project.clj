efe44891dce8443521c13d75758c3ebbb9effd63
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-01-02 15:41:23 +0000

    Make ready for next release cycle

--- project.clj
+++ project.clj
@@ -1,11 +1,16 @@
-(defproject com.floatbackwards/multipart "0.0.6"
+(defproject com.floatbackwards/multipart "0.0.7-SNAPSHOT"
   :description "Ring middleware for parsing multipart/mixed requests"
+
   :url "https://github.com/whostolebenfrog/compojure-multipart"
+
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.4.0"]
                  [org.apache.commons/commons-email "1.2"]
                  [commons-fileupload/commons-fileupload "1.2.2"]]
   :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :plugins [[lein-release "1.0.0"]]
 
