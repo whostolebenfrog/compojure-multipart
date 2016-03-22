152c78ccbd4830630ab03abe937c19d763df130e
Ben Griffiths <benjamin.griffiths@mixrad.io>
2016-03-22 21:41:29 +0000

    Version 0.0.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.floatbackwards/multipart "0.0.9"
+(defproject com.floatbackwards/multipart "0.0.10-SNAPSHOT"
   :description "Ring middleware for parsing multipart/mixed requests"
 
   :url "https://github.com/whostolebenfrog/compojure-multipart"
